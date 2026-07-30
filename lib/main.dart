9ce0bd72eca0363da1c337b5deafd52eb3cc6a5b
Emilly Neves <emillyneves280@gmail.com>
2026-07-30 19:57:35 -0300

    Implementa o funcionamento de push e pop nos links.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:primeiro_app/paginas/cadastro.dart';
 import 'package:primeiro_app/paginas/login.dart';
-import 'package:primeiro_app/utilitarios/tipografia.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // Tópico 2: Remove a faixa de debug
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(child: Login()),
-      ),
-    );
+    return MaterialApp(home: Login());
   }
 }
-
-
